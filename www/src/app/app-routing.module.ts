1293ee31f806ad25a5af26c45dd245edabeff054
Clovis Provost <clovisprovost@hotmail.com>
2022-02-15 07:49:12 -0500

    ajout de composante et de routing

--- www/src/app/app-routing.module.ts
+++ www/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CreateComponent } from './blog/create/create.component';
 
 
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'create', component: CreateComponent},
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
