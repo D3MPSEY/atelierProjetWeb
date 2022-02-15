d4d1420722eda79e8919e6ec0a156f018b92dedf
Clovis Provost <clovisprovost@hotmail.com>
2022-02-15 07:39:08 -0500

    Ajout de routing

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CreateComponent } from './blog/create/create.component';
+import { ListComponent } from './blog/list/list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
